4d419823bf8a1872886083d792115bd64d170cfe
Chris Hua <hua.christopher@gmail.com>
2022-07-07 11:22:53 -0700

    chore: refactor predicate operators

--- pkg/predicates/compound_predicate.go
+++ pkg/predicates/compound_predicate.go
@@ -64,60 +64,44 @@ func (cp *CompoundPredicate) UnmarshalXML(d *xml.Decoder, start xml.StartElement
 }
 
 func (p *CompoundPredicate) Evaluate(features map[string]interface{}) (bool, bool, error) {
-	// TODO: Refactor this.
-	switch p.Operator {
-	case op.Operators.And:
-		// The operator and indicates an evaluation to TRUE if all the predicates evaluate to TRUE.
-		for _, predicate := range p.Predicates {
-			eval, ok, err := predicate.Evaluate(features)
-			if err != nil {
-				return false, false, errors.Wrapf(err, "Error when evaluating predicate %s", p)
-			}
+	count := 0
+	for _, predicate := range p.Predicates {
+		eval, ok, err := predicate.Evaluate(features)
+		if err != nil {
+			return false, false, errors.Wrapf(err, "Error when evaluating predicate %s", p)
+		}
+		switch p.Operator {
+		case op.Operators.And:
+			// The operator and indicates an evaluation to TRUE if all the predicates evaluate to TRUE.
 			// if value is null (and this function returns False), we still return false
 			if !eval || !ok {
 				return false, true, nil
 			}
-		}
-		return true, true, nil
-	case op.Operators.Or:
-		// The operator or indicates an evaluation to TRUE if one of the predicates evaluates to TRUE.
-		for _, predicate := range p.Predicates {
-			eval, _, err := predicate.Evaluate(features)
-			if err != nil {
-				return false, false, errors.Wrapf(err, "Error when evaluating predicate %s", p)
-			}
+		case op.Operators.Or, op.Operators.Surrogate:
+			// The operator or indicates an evaluation to TRUE if one of the predicates evaluates to TRUE.
 			// even if some values are missing, we can still return true
+			// --
+			// The operator surrogate allows for specifying surrogate predicates.
+			// They are used for cases where a missing value appears in the evaluation of the parent predicate such that an alternative predicate is available.
 			if eval {
 				return true, true, nil
 			}
-		}
-		return false, false, nil
-	case op.Operators.Xor:
-		// The operator xor indicates an evaluation to TRUE if an odd number of the predicates evaluates to TRUE and all others evaluate to FALSE.
-		count := 0
-		for _, predicate := range p.Predicates {
-			eval, _, err := predicate.Evaluate(features)
-			if err != nil {
-				return false, false, errors.Wrapf(err, "Error when evaluating predicate %s", p)
-			}
+		case op.Operators.Xor:
+			// The operator xor indicates an evaluation to TRUE if an odd number of the predicates evaluates to TRUE and all others evaluate to FALSE.
 			if eval {
 				count++
 			}
+		default:
+			return false, false, fmt.Errorf("unsupported compound predicate operator: %s", p.Operator)
 		}
-		return count%2 == 1, true, nil
-	case op.Operators.Surrogate:
-		// The operator surrogate allows for specifying surrogate predicates.
-		// They are used for cases where a missing value appears in the evaluation of the parent predicate such that an alternative predicate is available.
-		for _, predicate := range p.Predicates {
-			eval, _, err := predicate.Evaluate(features)
-			if err != nil {
-				return false, false, errors.Wrapf(err, "Error when evaluating predicate %s", p)
-			}
-			if eval {
-				return true, true, nil
-			}
-		}
+	}
+	switch p.Operator {
+	case op.Operators.And:
+		return true, true, nil
+	case op.Operators.Or, op.Operators.Surrogate:
 		return false, false, nil
+	case op.Operators.Xor:
+		return count%2 == 1, true, nil
 	default:
 		return false, false, fmt.Errorf("unsupported compound predicate operator: %s", p.Operator)
 	}
